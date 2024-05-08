d65ae5a1b560db264e737bb0023b10c037504307
Jo HyunA <teniuscom@gmail.com>
2024-05-08 15:02:08 +0900

    linked_list.cpp의 size와 remove() 관련 오류 수정

--- linked_list.cpp
+++ linked_list.cpp
@@ -27,7 +27,7 @@ void LinkedList::print(){
 }
 
 void LinkedList::insert(int index, int value){
-    // index가 0인 경우 head가 바뀌어야 하니까 조심
+    // 주의 : index가 0인 경우 head가 바뀌어야 하니까 조심
     Node* node=new Node(value);
     if(index==0){
         // 새로운 노드가 head가 되고, 이전 head를 next_에 할당
@@ -43,6 +43,9 @@ void LinkedList::insert(int index, int value){
         node->next_=cur->next_;
         cur->next_=node;
     }
+    
+    // 노드 추가했으니 size 증가
+    size_++;
 }
 
 int LinkedList::get(int index){
@@ -56,12 +59,24 @@ int LinkedList::get(int index){
 }
 
 void LinkedList::remove(int index){
+    // 주의 : index가 0인 경우 head가 바뀌어야 하니까 조심
     // (index-1)번 링크를 타서 도착한 노드 뒤에 노드를 삭제
-    Node* cur=head_;
-    for(int i=0;i<index-1 && cur!=nullptr;i++){
-        cur=cur->next_;
+    Node* removeNode;
+    if(index==0){
+        // head를 삭제하는 경우 그냥 head 위치만 옮겨주고 삭제하면 된다
+        removeNode=head_;
+        head_=removeNode->next_;
     }
-    Node* removeNode=cur->next_;
-    cur->next_=removeNode->next_;
+    else{
+        Node* cur=head_;
+        for(int i=0;i<index-1 && cur!=nullptr;i++){
+            cur=cur->next_;
+        }
+        removeNode=cur->next_;
+        cur->next_=removeNode->next_;
+    }
     delete removeNode;
+
+    // 노드 삭제했으니 size 감소
+    size_--;
 }
