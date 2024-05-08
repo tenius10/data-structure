25a9eb17e4cb34ad64c67442cb17f6de7131990e
Jo HyunA <teniuscom@gmail.com>
2024-05-08 14:49:06 +0900

    list 완성

--- list.cpp
+++ list.cpp
@@ -1,3 +1,18 @@
 #include "list.h"
 
 // TODO: List 클래스 구현 작성
+
+// index 위치에 노드를 삽입한다
+void List::insert(int index, int value){
+    LinkedList::insert(index, value);
+}
+
+// index에 위치한 노드의 값을 반환한다
+int List::get(int index){
+    return LinkedList::get(index);
+}
+
+// index에 위치한 노드를 제거하고 메모리 상에서 해제한다.
+void List::remove(int index){
+    LinkedList::remove(index);
+}
--- list.h
+++ list.h
@@ -3,4 +3,18 @@
 
 // TODO: List 클래스 정의 작성
 
+#include "linked_list.h"
+
+class List : public LinkedList{
+public:
+    // index 위치에 노드를 삽입한다
+    void insert(int index, int value);
+
+    // index에 위치한 노드의 값을 반환한다
+    int get(int index);
+    
+    // index에 위치한 노드를 제거하고 메모리 상에서 해제한다.
+    void remove(int index);
+};
+
 #endif
